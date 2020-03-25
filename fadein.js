dbaac7e6f3ea3430008f1f931dbe5c66e100ad50
yuuka-lav <60505747+yuuka-lav@users.noreply.github.com>
2020-03-25 20:34:45 +0900

    add frontpage
    portfolio以外の作成
    お問い合わせホームの調節

---
+++ fadein.js
@@ -0,0 +1,13 @@
+$(function(){
+  $(window).scroll(function (){
+      $('.fadein').each(function(){
+          var targetElement = $(this).offset().top;
+          var scroll = $(window).scrollTop();
+          var windowHeight = $(window).height();
+          if (scroll > targetElement - windowHeight + 200){
+              $(this).css('opacity','1');
+              $(this).css('transform','translateY(0)');
+          }
+      });
+  });
+});
